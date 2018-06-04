fb9183a63591b682142e8cd8119f8ef23bedaa5d
billy1816 <billy1816_@hotmail.com>
2018-06-04 22:30:37 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $.ajax({
         type: "GET",
-        url: "https://mysafeinfo.com/api/data?list=englishmonarchs&format=xml",
+        url: "https://rawgit.com/billy1816/Examen-2018/master/xml/preguntas.xml",
         dataType: "xml",
         success: function (result) {
-            $(result).find("e").each(function () {
-                $("#presidents").append($("<option />").val($(this).attr("cty")).text($(this).attr("nm")));
+            $(result).find("question").each(function () {
+                $("#presidents").append($("<option />").val($(this).attr("id")).text($(this).attr("title")));
             });
         }
     });
