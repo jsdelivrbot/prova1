be3b293888e33591fdc37f04c2de239f467989a5
billy1816 <billy1816_@hotmail.com>
2018-06-04 22:24:15 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $.ajax({
         type: "GET",
-        url: "https://rawgit.com/billy1816/prova1/master/json/json.json",
+        url: "https://rawgit.com/billy1816/Examen-2018/master/xml/preguntas.xml",
         dataType: "xml",
         success: function (result) {
-            $(result).find("stateid").each(function () {
-                $("#presidents").append($("<option />").text($(this).attr("statename")));
+            $(result).find("question").each(function () {
+                $("#presidents").append($("<option />").val($(this).attr("id")).text($(this).attr("tittle")));
             });
         }
     });
